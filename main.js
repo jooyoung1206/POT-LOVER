b5cb719fbc0eabd23722dc01207d7281ac2e293e
joo <hanjooyoung1206>
2018-03-27 11:05:48 +0900

    https://github.com/jooyoung1206/1114.git

---
+++ main.js
@@ -0,0 +1,9 @@
+var myHeading = document.querySeleactor('h1');
+myHeading.innerHTML = 'Cloude server<br>2018-1!';
+var text = 'This is a text.';
+text = 10.5;
+var a = 33.7;
+var b = a*text;
+var iceCream = 'chocolate';
+var id =prompt('ID 입력', '');
+alert(id + '가 로그인함.');
