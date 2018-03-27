baa0edcc7e7a807f994ff767b13f2ea1a5b364d5
joo <hanjooyoung1206>
2018-03-27 11:52:03 +0900

    https://github.com/jooyoung1206/mokwon.git

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 var myHeading = document.querySeleactor('h1');
 myHeading.innerHTML = 'Cloude server<br>2018-1!';
-var text = 'This is a text.';
+
+function getIdPw() { 
 text = 10.5;
-var a = 33.7;
-var b = a*text;
-var iceCream = 'chocolate';
-var id =prompt('ID 입력', '');
-alert(id + '가 로그인함.');
-var passwordSystem = '12345';
-var password = prompt('PW 입력', '');
-if (password === passwordSystem) alert('정상 로그인!');
-else alert('비밀번호 오류');
+    var id =prompt('ID 입력', '');
+    alert(id + '가 로그인함.');
+    var passwordSystem = '12345';
+    var password = prompt('PW 입력', '');
+    if (password === passwordSystem) alert('정상 로그인!');
+    else alert('비밀번호 오류');
+}
+
+
+document.querySelector('html').onclick = function () {
+    getIdPw();
+}
+
+var myImage = document.querySelector('img');
+myImage.onclick = function () {
+var src = myImage.getAttribute('src');
+if (src === 'images/firefox-icon.png')
+    myImg.setAttribute('src', 'images/mokwon.png');
+else
+    myImg.setAtrribute('src', 'images/firefox-icon.png');
+    
