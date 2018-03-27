e69427d9a5708062e42773640425680f4580d0b4
joo <hanjooyoung1206>
2018-03-27 13:01:17 +0900

    1987

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,37 @@
-var myHeading = document.querySeleactor('h1');
-var id = localStorage.getItem('id');
-myHeading.innerHTML = id + 'Homepage';
+var myHeading = document.querySelector('h1');  
+myHeading.innerHTML = 'Cloud Service<br>2018-1';  
+var id = localStorage.getItem('id');  
+myHeading.innerHTML = id + ' Homepage';  
+function getIdPw() {  
+    var id = prompt('ID 입력', '');  
+    alert(id + '가 로그인함.');  
+    localStorage.setItem('id', id);  
+    var passwordSystem = '12345';  
+    var password = prompt('PW 입력', '');  
+    if (password === passwordSystem) alert('정상 로그인!');  
+    else alert('비밀번호 오류');  
+}  
 
-function getIdPw() {    
-text = 10.5;
-    var id =prompt('ID 입력', '');
-    alert(id + '가 로그인함.');
-    localStorage.setItem('id', id);
-    var passwordSystem = '12345';
-    var password = prompt('PW 입력', '');
-    if (password === passwordSystem) alert('정상 로그인!');
-    else alert('비밀번호 오류');
-}
 
 
-document.querySelector('html').onclick = function () {
-    getIdPw();
-}
+document.querySelector('h1').onclick = function () {  
+    getIdPw();  
+}  
+    
 
-var myImage = document.querySelector('img');
-myImage.onclick = function () {
-var src = myImage.getAttribute('src');
-if (src === 'images/firefox-icon.png') {
-    myImg.setAttribute('src', 'images/mokwon.png');
-} else {
-    myImg.setAtrribute('src', 'images/firefox-icon.png');
+var myImg = document.querySelector('img');  
+myImg.onclick = function () {  
+    var src = myImg.getAttribute('src');  
+    if (src === 'images/firefox-icon.png')  
+    myImg.setAttribute('src', 'images/mokwon.png');  
+else  
+    myImg.setAttribute('src', 'images/firefox-icon.png');  
+
     }
-}
 
-var myButton = document.querySelector('button');
-myButton.onclick = function ( {
-    getIdPw();
+
+var myButton = document.querySelector('button');  
+    myButton.onclick = function () {  
+    getIdPw();  
+
 }
